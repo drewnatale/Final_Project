b378c7dacbdc50314f8cbfc7e40b86b93256f965
drewnatale <nataled0435@my.sduhsd.net>
2017-05-10 10:30:05 -0700

    Fixed Errors

--- FinalProject-Period#3/Jade'sJavascriptEdits.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by BabcockChij1158 on 5/10/2017.
- */
---
+++ Final_Project/FinalProj-file1.js
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+    <title>Canvas</title>
+    <head>
+    <style>
+#canvas{
+    outline: 1px solid red;
+}
+</style>
+</head>
+<body>
+
+<canvas id="canvas" height="400" width="600"></canvas>
+
+    <script>
+var canvas = document.querySelector('#canvas');
+var context = canvas.getContext('2d');
+
+var xpos = 0;
+var ypos = 0;
+
+cantext.rect(xPos, yPos, 50, 50);
+context.stroke();
+
+function move(e){
+
+    if(e.keyCode==39){
+        xPos+=5;
+    }
+    if(e.keyCode==37){
+        xPos-=5;
+    }
+    if(e.keyCode == 38){
+        yPos-=5;
+    }
+    if(e.keyCode == 40){
+        yPos+=5;
+    }
+
+    canvas.width=canvas.width;
+    context.rect(xPos, ypos, 50, 50);
+    context.stroke();
+}
+
+document.onkeydown = move;
+</script>
+</body>
+</html>
