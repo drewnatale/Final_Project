0790d904782bb7d20f7fafef43c044bfae3a312f
BabcockChij1158 <jadebc01@gmail.com>
2017-05-12 10:25:34 -0700

    need to create replacement loop to move

---
+++ FinalProject-Period#3/JadeJS_snakeloop.js
@@ -0,0 +1,14 @@
+/**
+ * Created by BabcockChij1158 on 5/12/2017.
+ */
+/**var canvas = ("#canvas")[0]**/
+var w = ("#canvas").width();
+var h = ("#canvas").height();
+var x = document.getElementById("canvas");
+canvas = x.getContext("2d");
+
+
+
+var cellWidth = 10;
+var food;
+var score;
