33f0523c677070f277e34ef0a81aede58ead394c
BabcockChij1158 <jadebc01@gmail.com>
2017-05-22 10:26:06 -0700

    on video 3 of 4 12:15 in

--- FinalProject-Period#3/SnakeVideoJS_jade.js
+++ FinalProject-Period#3/SnakeVideoJS_jade.js
@@ -15,8 +15,8 @@ var tailY = [snakeY];
 //coordinates of the fruit/food
 var fX;
 var fY;
-var running;
-var gameOver;
+var running = false;
+var gameOver = false;
 var direction = -1; //up = 0, down = -1, left  = 1, right  = 2, can change this later if want
 var interval = 100;
 var int; //short for interval, this is the identifier for the interval we set eariler
@@ -43,15 +43,90 @@ function createMap(){
         document.write("<tr>");
         for(var x = 0;  x < width; x++){
             if(x == 0 || x == width -1 || y == 0 || y == height -1){
-                document.write("<td class = "wall" id='"+x+"-"+y+"'></td>");
+                document.write("<td class = 'wall' id='"+x+"-"+y+"'></td>");
             }else{
-                document.write("<td class = "blank" id='"+x+"-"+y+"'></td>");
+                document.write("<td class = 'blank' id='"+x+"-"+y+"'></td>");
 
             }	//checks that if block we are placing is a wall
         }
-        document.write("</tr>">);
+        document.write("</tr>");
     }
     document.write("</table>");
+}
+
+function createSnake(){
+    set(snakeX, snakeY, "snake");
+}
+
+function get(x,y){
+    return document.getElementById(x+"-"+y);
+}
+
+function set(x,y,value){
+    get(x,y).setAttribute("class",value);
+}
+
+function rand(min, max){
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+function getType(x,y){
+    return get(x,y).getAttribute("class");
+}
+
+function createFruit(){
+    var found = false;
+    while(!found && (length < (width-2)*(height-2)+1)){  //something in here makes sure the snake or fruit is not the volume of the entire page so it does not create an infinite loop
+        var fruitX = rand(1,width-1);
+        var fruitY  = rand(1,height-1);
+        if(getType(fruitX, fruitY)=="blank")
+            found = true;
+
+    }
+    set(fruitX, fruitY, "fruit"); //set to fruit type
+    fX = fruitX;
+    fY = fruitY;
+}
+
+window.addEventListener("keypress", function key(){
+    //if key is W set direction
+    var key = event.keyCode;//want to restrict them from going strait from down to up
+    if(direction != -1 && (key == 119 || key == 87)) //key codes for upper and lower case "w"
+        direction = 0;
+    //if key is S set direction down;
+    else if(direction != 0 && (key == 115 || key == 83)) //key codes for upper and lower case "s"
+        direction = -1; //sets direction down
+    //if key is A set direction left
+    else if(direction != 2 && (key == 97 || key == 65)) //key codes for upper and lower case "a"
+        direction = 1; //sets direction left
+    //if key is D set direction right
+    else if(direction != 1 && (key == 100 || key == 68)) //key codes for upper and lower case "d"
+        direction = 2; //sets direction right
+    if(!running)
+        running = true;
+    else if(key == 32)//space bar
+        running = false; // will allow you to pause game without restarting
+
+
+
+});
+
+function gameLoop(){
+    if(running && !gameOver){
+        update();
+    }else if(gameOver){
+        clearInterval(interval);
+    }
+}
+
+function update(){
+    setBlock(fX, fY, "fruit");
+    set(tailX[length], tailY[length], "blank");
+    if(direction == 0){}
+
+
+
+
 }
 
 run();
